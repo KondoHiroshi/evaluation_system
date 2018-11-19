4929ce1d42a43a717ffcc980851a45cdf6ccf582
ogawahideo <ogawahideo@MacBook-Air.local>
2018-11-19 17:02:34 +0900

    from

--- scripts/sis_iv_measure.py
+++ scripts/sis_iv_measure.py
@@ -10,7 +10,7 @@ from std_msgs.msg import String
 from std_msgs.msg import Int32
 
 sys.path.append("/home/amigos/ros/src/evaluation_system/scripts")
-import iv_readear
+from . import iv_readear
 
 class sis_iv(object):
     def __init__(self):
