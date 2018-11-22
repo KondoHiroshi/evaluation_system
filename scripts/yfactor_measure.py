f140ef3fe27f83600aed82a7ee4d4d33c7e7560c
ogawahideo <ogawahideo@MacBook-Air.local>
2018-11-22 19:53:56 +0900

    a

--- scripts/yfactor_measure.py
+++ scripts/yfactor_measure.py
@@ -61,7 +61,7 @@ class yfactor(object):
 
 if __name__ == "__main__" :
     rospy.init_node("yfactor_measure")
-    reader = sis_reader.piv_reader()
+    reader = reader.piv_reader()
     yf = yfactor()
     initv = int(input("start_voltage = ? [mV]"))
     lastv = int(input("finish_voltage = ? [mV]"))
