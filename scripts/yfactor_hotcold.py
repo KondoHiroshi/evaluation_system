d76cad41e903bb6ce0b8eda07322ff4e17033130
ogawahideo <ogawahideo@MacBook-Air.local>
2018-11-28 21:26:37 +0900

    label

--- scripts/yfactor_hotcold.py
+++ scripts/yfactor_hotcold.py
@@ -71,7 +71,7 @@ class yfactor(object):
         ax1.set_ylabel("current[uA]")
         ax2.set_ylabel("power[dBm]")
         ax1.legend(loc='upper left')
-        ax2.legend(loc='upper left')
+        ax2.legend(loc='upper right')
         plt.savefig("yfactor_{0}.png".format(self.ut))
         plt.show()
 
