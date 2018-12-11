870e199083126d072437623af6ecf1242a54e4e0
ogawahideo <ogawahideo@MacBook-Air.local>
2018-12-11 21:59:13 +0900

    0.05

--- scripts/tz_sis_iv_measure.py
+++ scripts/tz_sis_iv_measure.py
@@ -31,7 +31,7 @@ class sis_iv(object):
         self.pub_vol_ch2.publish(initv)
         self.pub_vol_ch3.publish(initv)
         self.pub_vol_ch4.publish(initv)
-        time.sleep(0.1)
+        time.sleep(0.3)
         for i in range(repeat+1):
             da = []
             vol = initv+interval*i
@@ -41,7 +41,7 @@ class sis_iv(object):
             self.pub_vol_ch2.publish(vol)
             self.pub_vol_ch3.publish(vol)
             self.pub_vol_ch4.publish(vol)
-            time.sleep(0.1)
+            time.sleep(0.05)
             ret = reader.iv_reader()
             time.sleep(0.01)
             da.append(ret[0])
